5c7fd5ceccce3427d714e134a7245b3828ac97ea
Lahiru illangasinghe <146197258+LAHIRUKBI@users.noreply.github.com>
2026-09-23 21:38:54 +0530

    Add updates

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Controller ගොනුවෙන් functions ඉම්පෝර්ට් කරගැනීම
-const {addTeacher,getAllTeachers,getAllAdmins,deleteAdmin,deleteTeacher} = require('../controllers/adminController');
+const {addTeacher,getAllTeachers,getAllAdmins,deleteAdmin,deleteTeacher,getAllStudents} = require('../controllers/adminController');
 
 // Routes නිර්මාණය කිරීම
 router.post('/add-teacher', authMiddleware, addTeacher);
@@ -11,5 +11,6 @@ router.get('/teachers', authMiddleware, getAllTeachers);
 router.get('/admins', authMiddleware, getAllAdmins);
 router.delete('/admins/:id', authMiddleware, deleteAdmin);
 router.delete('/teachers/:id', authMiddleware, deleteTeacher);
+router.get('/students', getAllStudents);
 
 module.exports = router;
